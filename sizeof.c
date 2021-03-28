c37d1d19b94a2df881544161f87f313f7c14b9a5
Erisson Orsso <erisson.orsso@gmail.com>
2021-03-28 15:58:32 +0100

    Variable's practice

---
+++ sizeof.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("sizeof(int): %d", sizeof(int));
+}
---
+++ variable-3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	char c = 'a';
+	float x = 12345678.9876;
+	double y = 12345678.9876;
+
+	//show values
+	printf("c: %c (%d), x: %f, y: %g", c, c, x, y);
+
+}
